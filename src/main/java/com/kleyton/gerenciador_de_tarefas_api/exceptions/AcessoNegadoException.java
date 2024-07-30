e9babe22211154d843c0cef4cd77bb8fd7e707f5
KleytonFMadruga <kleytonmadruga925@gmail.com>
2024-07-30 16:03:25 -0300

    Suprime warning sem utilidade

--- src/main/java/com/kleyton/gerenciador_de_tarefas_api/exceptions/AcessoNegadoException.java
+++ src/main/java/com/kleyton/gerenciador_de_tarefas_api/exceptions/AcessoNegadoException.java
@@ -1,5 +1,6 @@
 package com.kleyton.gerenciador_de_tarefas_api.exceptions;
 
+@SuppressWarnings("serial")
 public class AcessoNegadoException extends RuntimeException {
 	public AcessoNegadoException(String message) {
 		super(message);
